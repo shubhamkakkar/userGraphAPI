6cd3058e0028a867d4309e5c6f344aa3708854f9
shubhamkakkar <shubham.kakkar98@gmail.com>
2020-03-21 01:24:28 +0530

    previous user auto login on signin added

--- server/resolver/signInMutation/signInUser.ts
+++ server/resolver/signInMutation/signInUser.ts
@@ -2,13 +2,28 @@ import bcrypt  from "bcrypt";
 import jwt from "jsonwebtoken"
 import {TUserCredential} from "../../types";
 import UserModal from "../../models/userModel";
+import {UserInputError} from "apollo-server";
 const saltRounds = 10;
 export default function signInUser({ email,password }:TUserCredential ) {
     return UserModal.findOne({ email })
-        .then( user => {
+        .then( (user) => {
             if(user){
-                // user exists
-                console.log({user})
+                // @ts-ignore
+                  const {password: hashedPassword} = user._doc;
+                  return bcrypt.compare(password, hashedPassword)
+                      .then(passwordCheckBool =>{
+                          if(passwordCheckBool){
+                              const token =  jwt.sign({email}, "string");
+                              return {
+                                  token,
+                                  // @ts-ignore
+                                  ...user._doc,
+                              }
+                          }else{
+                              throw new UserInputError('Passwords Dont Match', {invalidArgs: password});
+                          }
+                      })
+                      .catch(er => er);
             }else{
                 // create new user
                 return bcrypt.hash(password, saltRounds)
